7310859c6f5ca94b779ca4476938981a4ab7f98e
patrasxd <patrasxd@gmail.com>
2023-06-06 22:04:02 +0200

    some chngs

--- src/asi/preprocessing/prepare_data.py
+++ src/asi/preprocessing/prepare_data.py
@@ -1,14 +1,21 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 from asi.uttils.helpers import object_to_int
 
 
 def prepare_data(input_file: str, output_file: str):
+    # Tworzenie instancji obiektu MinMaxScaler
+    scaler = MinMaxScaler()
     data = pd.read_csv(input_file)
     data["TotalCharges"] = pd.to_numeric(data.TotalCharges, errors="coerce")
     data.dropna(inplace=True)
     data = data.drop(["customerID"], axis=1)
     data.drop(labels=data[data["tenure"] == 0].index, axis=0, inplace=True)
     data["SeniorCitizen"] = data["SeniorCitizen"].map({0: "No", 1: "Yes"})
+    # minMax
+    data["tenure"] = scaler.fit_transform(data[["tenure"]])
+    data["MonthlyCharges"] = scaler.fit_transform(data[["MonthlyCharges"]])
+    data["TotalCharges"] = scaler.fit_transform(data[["TotalCharges"]])
     data = data.apply(lambda x: object_to_int(x))
     data.to_csv(output_file, index=False)
--- src/asi/training/train_model.py
+++ src/asi/training/train_model.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
+import pandas_profiling
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
 import csv
 import wandb
 import h2o
@@ -19,11 +22,32 @@ def train_model(processed_data_path: str, evaluation_metrics_path: str, model_pa
     print(f"best_model: {best_model}")
 
     data = pd.read_csv(processed_data_path)
+    # report
+    profile = data.profile_report()
+    profile.to_file("data_profile_report.html")
     X = data.drop(columns=["Churn"])
     y = data["Churn"].values
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.30, random_state=40, stratify=y
     )
+    # Definicja siatki parametrów do przeszukania
+    param_grid = {
+        'n_estimators': [100, 200, 300],
+        'max_depth': [None, 5, 10],
+        'min_samples_split': [2, 5, 10]
+    }
+    # Inicjalizacja modelu
+    model = RandomForestClassifier()
+
+    # Użycie GridSearchCV do przeszukania siatki parametrów
+    grid_search = GridSearchCV(model, param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+
+    # Najlepsze parametry i najlepszy model
+    best_params = grid_search.best_params_
+    best_model = grid_search.best_estimator_
+    print(f"Best model {best_model}")
+
     knn_model = KNeighborsClassifier(n_neighbors=11)
     knn_model.fit(X_train, y_train)
     predicted_y = knn_model.predict(X_test)
