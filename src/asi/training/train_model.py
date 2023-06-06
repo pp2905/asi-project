9c6b91ea93aafd5a67080051e551c2209af8c52f
Osucha <mosucha99@gmail.com>
2023-06-06 20:27:50 +0200

    pkt 4 i pol 5

--- src/asi/training/train_model.py
+++ src/asi/training/train_model.py
@@ -4,8 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
 import csv
+import wandb
+import h2o
 
 def train_model(processed_data_path: str, evaluation_metrics_path: str, model_path: str):
+    # Inicjalizacja projektu Weights and Biases
+    wandb.init(project='my-awesome-project', entity='asi-project-2023')
+
+    h2o.init()
+    data = h2o.import_file(processed_data_path)
+    automl = h2o.automl.H2OAutoML(max_models = 10)
+    automl.train(y="Churn", training_frame=data)
+    best_model = automl.leader
+    print(f"best_model: {best_model}")
+
     data = pd.read_csv(processed_data_path)
     X = data.drop(columns=["Churn"])
     y = data["Churn"].values
@@ -18,8 +30,10 @@ def train_model(processed_data_path: str, evaluation_metrics_path: str, model_pa
     accuracy_knn = knn_model.score(X_test, y_test)
     print("KNN accuracy:", accuracy_knn)
     now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    wandb.log({'loss': now, 'accuracy': accuracy_knn})
     with open(evaluation_metrics_path, 'a') as f:
         row = [now, 1.0, "KNeighborsClassifier", accuracy_knn]
         writer = csv.writer(f, lineterminator='\n')
         writer.writerow(row)
     pickle.dump(knn_model, open(model_path, "wb"))
+    wandb.finish()
