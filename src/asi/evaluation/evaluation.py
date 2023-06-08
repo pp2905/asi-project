6adeb48e1f8dde63e170a37ead0b5df49cfb1d54
patrasxd <patrasxd@gmail.com>
2023-06-08 21:22:36 +0200

    wandb - full implementation

--- src/asi/evaluation/evaluation.py
+++ src/asi/evaluation/evaluation.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import numpy as np
 
 import matplotlib.pyplot as plt
 import wandb
@@ -9,12 +10,20 @@ from sklearn.metrics import roc_curve, roc_auc_score
 def evaluate_model(model, X_test, y_test, evaluation_metrics_path):
     # Ocena modelu na zbiorze testowym
     y_pred = model.predict(X_test)
+    y_probs = model.predict_proba(X_test)
     accuracy = model.score(X_test, y_test)
     print("Accuracy:", accuracy)
 
     now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
-    wandb.log({"accuracy": accuracy})
+    # wandb.log({"accuracy": accuracy})
+    wandb.summary["accuracy"] = accuracy
+    print(wandb.run.name)
+    run_name = f"Confusion_Matrix_{wandb.run.name}"
+    wandb.log({run_name:wandb.sklearn.plot_confusion_matrix(y_test, y_pred, labels=["No","Yes"])})
+    # y_test_arr = np.array(y_test)
+    # y_probs_arr = np.array(y_probs)
+    wandb.sklearn.plot_roc(y_test, y_probs, labels=["No","Yes"])
 
     with open(evaluation_metrics_path, "a") as f:
         row = [now, 1.0, str(model), accuracy]
@@ -22,18 +31,18 @@ def evaluate_model(model, X_test, y_test, evaluation_metrics_path):
         writer.writerow(row)
 
 
-def plot_roc_curve(model, X_test, y_test):
-    # Tworzenie krzywej ROC
-    y_probs = model.predict_proba(X_test)[:, 1]
-    fpr, tpr, thresholds = roc_curve(y_test, y_probs)
-    auc = roc_auc_score(y_test, y_probs)
-
-    # Rysowanie wykresu
-    plt.figure()
-    plt.plot(fpr, tpr, label=f"AUC = {auc:.2f}")
-    plt.plot([0, 1], [0, 1], "k--")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("Receiver Operating Characteristic")
-    plt.legend(loc="lower right")
-    plt.savefig(f"data/evaluation/{str(model)}.png")
+# def plot_roc_curve(model, X_test, y_test):
+#     # Tworzenie krzywej ROC
+#     y_probs = model.predict_proba(X_test)[:, 1]
+#     fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+#     auc = roc_auc_score(y_test, y_probs)
+#
+#     # Rysowanie wykresu
+#     plt.figure()
+#     plt.plot(fpr, tpr, label=f"AUC = {auc:.2f}")
+#     plt.plot([0, 1], [0, 1], "k--")
+#     plt.xlabel("False Positive Rate")
+#     plt.ylabel("True Positive Rate")
+#     plt.title("Receiver Operating Characteristic")
+#     plt.legend(loc="lower right")
+#     plt.savefig(f"data/evaluation/{str(model)}.png")
--- src/asi/training/train_model.py
+++ src/asi/training/train_model.py
@@ -7,20 +7,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-from asi.evaluation.evaluation import evaluate_model, plot_roc_curve
+from asi.evaluation.evaluation import evaluate_model
 from asi.evaluation.hyperparameter_tuning import tune_hyperparameters
 from asi.models.decision_tree import build_decision_tree
 from asi.models.random_forest import build_random_forest
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
+wandb.login(key="9d8b84a3274d42706670aab35313c6333e638c5b")
 
-
-def train_model(
-    processed_data_path: str, evaluation_metrics_path: str, model_path: str
+def make_dataset(
+    processed_data_path: str
 ):
-    wandb.login(key="9d8b84a3274d42706670aab35313c6333e638c5b")
-    wandb.init(project="my-awesome-project", entity="asi-project-2023")
-
     df = pd.read_csv(processed_data_path)
 
     # Kodowanie binarne dla kolumny 'Churn'
@@ -33,37 +30,93 @@ def train_model(
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
+    return X_train, X_test, y_train, y_test
 
+
+def run_sweep_random_forest(
+    X_train, X_test, y_train, y_test, evaluation_metrics_path: str, model_path
+):
+    def evaluate_random_forest():
+        wandb.init(tags=["RandomForestModel"])
+        # Dostosowanie hiperparametrów dla RandomForestClassifier
+        # random_forest_model = build_random_forest(X_train, y_train)
+        # param_grid = {"n_estimators": [100, 200, 300], "max_depth": [None, 5, 10]}
+        # tune_hyperparameters(random_forest_model, X_train, y_train, param_grid)
+
+        # Budowanie modelu RandomForestClassifier z optymalnymi hiperparametrami
+        print(wandb.config)
+        random_forest_model = build_random_forest(
+            X_train, y_train, n_estimators=wandb.config["n_estimators"], max_depth=wandb.config["max_depth"]
+        )
+
+        # Ocena modelu RandomForestClassifier
+        evaluate_model(random_forest_model, X_test, y_test, evaluation_metrics_path)
+        pickle.dump(random_forest_model, open(model_path["random_forest_model_path"], "wb"))
+        wandb_config_dict = {"n_estimators":wandb.config["n_estimators"], "max_depth":wandb.config["max_depth"]}
+        experiment_artifact = wandb.Artifact(
+            "random_forest_model", type='model',
+            description='Random Forest Model',
+            metadata=wandb_config_dict
+        )
+        experiment_artifact.add_file(model_path["random_forest_model_path"])
+        wandb.log_artifact(experiment_artifact)
+        wandb.run.link_artifact(experiment_artifact, "asi-project-2023/my-awesome-project/RandomForestModel")
+
+    sweep_config = {
+        'method': 'grid',
+        'metric': {'goal': 'maximize', 'name': 'accuracy'},
+        'parameters':
+            {
+                "n_estimators": {"values": [100, 200, 300]},
+                "max_depth": {"values": [None, 5, 10]},
+            }
+    }
+    sweep_id = wandb.sweep(project="my-awesome-project", entity="asi-project-2023", sweep=sweep_config)
+    wandb.agent(sweep_id, function=evaluate_random_forest, count=100)
+
+def train_model(
+        processed_data_path: str, evaluation_metrics_path: str, model_path: str
+):
+    X_train, X_test, y_train, y_test = make_dataset(processed_data_path)
+    run_sweep_random_forest(X_train, X_test, y_train, y_test, evaluation_metrics_path, model_path)
+    wandb.init(project="my-awesome-project", entity="asi-project-2023", tags=["LogisticModel"])
     # Budowanie modelu regresji logistycznej
     logistic_model = LogisticRegression()
     logistic_model.fit(X_train, y_train)
 
     # Ocena modelu LogisticRegression
     evaluate_model(logistic_model, X_test, y_test, evaluation_metrics_path)
-    plot_roc_curve(logistic_model, X_test, y_test)
+    # plot_roc_curve(logistic_model, X_test, y_test)
 
-    # Dostosowanie hiperparametrów dla RandomForestClassifier
-    random_forest_model = build_random_forest(X_train, y_train)
-    param_grid = {"n_estimators": [100, 200, 300], "max_depth": [None, 5, 10]}
-    tune_hyperparameters(random_forest_model, X_train, y_train, param_grid)
-
-    # Budowanie modelu RandomForestClassifier z optymalnymi hiperparametrami
-    random_forest_model_optimal = build_random_forest(
-        X_train, y_train, n_estimators=200, max_depth=10
-    )
-
-    # Ocena modelu RandomForestClassifier
-    evaluate_model(random_forest_model_optimal, X_test, y_test, evaluation_metrics_path)
-    plot_roc_curve(random_forest_model_optimal, X_test, y_test)
-
+    wandb.init(project="my-awesome-project", entity="asi-project-2023", tags=["DecisionTreeModel"])
     # Budowanie modelu DecisionTreeClassifier
     decision_tree_model = build_decision_tree(X_train, y_train)
 
     # Ocena modelu DecisionTreeClassifier
     evaluate_model(decision_tree_model, X_test, y_test, evaluation_metrics_path)
-    plot_roc_curve(decision_tree_model, X_test, y_test)
+    # plot_roc_curve(decision_tree_model, X_test, y_test)
 
-    pickle.dump(logistic_model, open(model_path, "wb"))
+    pickle.dump(logistic_model, open(model_path["logistic_model_path"], "wb"))
+    pickle.dump(decision_tree_model, open(model_path["decision_tree_model_path"], "wb"))
+    # artefakt decision tree
+    wandb_config_dict = {"n_estimators": wandb.config["n_estimators"], "max_depth": wandb.config["max_depth"]}
+    experiment_artifact = wandb.Artifact(
+        "decision_tree_model", type='model',
+        description='Decision Tree Model',
+        metadata=wandb_config_dict
+    )
+    experiment_artifact.add_file(model_path["decision_tree_model_path"])
+    wandb.log_artifact(experiment_artifact)
+    wandb.run.link_artifact(experiment_artifact, "asi-project-2023/my-awesome-project/DecisionTreeModel")
+    # artfetakt logistic model
+    wandb_config_dict = {"n_estimators": wandb.config["n_estimators"], "max_depth": wandb.config["max_depth"]}
+    experiment_artifact = wandb.Artifact(
+        "logistic_model", type='model',
+        description='Logistic Regression Model',
+        metadata=wandb_config_dict
+    )
+    experiment_artifact.add_file(model_path["logistic_model_path"])
+    wandb.log_artifact(experiment_artifact)
+    wandb.run.link_artifact(experiment_artifact, "asi-project-2023/my-awesome-project/LogisticModel")
 
     wandb.finish()
-
